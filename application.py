bc077f24497d286d806c438e4d9ba23443561a45
Aryaman Tamotia <102788424+AryamanTamotia@users.noreply.github.com>
2023-02-26 19:05:39 +0530

    Update application.py

--- application.py
+++ application.py
@@ -25,65 +25,66 @@ def index():
             uClient.close()
             flipkart_html = bs(flipkartPage, "html.parser")
             bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
-            del bigboxes[0:3]
-            box = bigboxes[0]
-            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
-            prodRes = requests.get(productLink)
-            prodRes.encoding='utf-8'
-            prod_html = bs(prodRes.text, "html.parser")
-            print(prod_html)
-            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
-
-            filename = searchString + ".csv"
-            fw = open(filename, "w")
-            headers = "Product, Customer Name, Rating, Heading, Comment \n"
-            fw.write(headers)
             reviews = []
-            for commentbox in commentboxes:
-                try:
-                    #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+            client = pymongo.MongoClient("mongodb+srv://atamotia:atamotia@cluster0.av6ep6v.mongodb.net/?retryWrites=true&w=majority")
+            db = client['Web_Scrapper']
+            db_coll = db[searchString]
 
+            for box in bigboxes:
+
+                try: 
+                    productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
+                    prodRes = requests.get(productLink)
+                    prodRes.encoding='utf-8'    
+                    prod_html = bs(prodRes.text, "html.parser")
+
+                    try:
+                            prodName = prod_html.find_all('span', {'class': 'B_NuCI'})[0].text
+                    
+                    except:
+                            prodName = 'No Name'
+
+                    commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
+                    for commentbox in commentboxes:
+
+                        try:
+                            name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+
+                        except:
+                            name = 'No Name'
+
+                        try:
+                            rating = commentbox.div.div.div.div.text
+
+                        except:
+                            rating = 'No Rating'
+
+                        try:
+                            commentHead = commentbox.div.div.div.p.text
+
+                        except:
+                            commentHead = 'No Comment Heading'
+                        
+                        try:
+                            comtag = commentbox.div.div.find_all('div', {'class': ''})
+                            custComment = comtag[0].div.text
+                        
+                        except Exception as e:
+                            comtag = 'No Comment'
+
+                        mydict = {"Product": prodName, "Name": name, "Rating": rating, "CommentHead": commentHead,
+                                "Comment": custComment}
+                        reviews.append(mydict)
+                
                 except:
-                    name = 'No Name'
-
-                try:
-                    #rating.encode(encoding='utf-8')
-                    rating = commentbox.div.div.div.div.text
-
-
-                except:
-                    rating = 'No Rating'
-
-                try:
-                    #commentHead.encode(encoding='utf-8')
-                    commentHead = commentbox.div.div.div.p.text
-
-                except:
-                    commentHead = 'No Comment Heading'
-                try:
-                    comtag = commentbox.div.div.find_all('div', {'class': ''})
-                    #custComment.encode(encoding='utf-8')
-                    custComment = comtag[0].div.text
-                except Exception as e:
-                    print("Exception while creating dictionary: ",e)
-
-                mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
-                          "Comment": custComment}
-                reviews.append(mydict)
-            client = pymongo.MongoClient("mongodb+srv://pwskills:pwskills@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
-            db = client['review_scrap']
-            review_col = db['review_scrap_data']
-            review_col.insert_many(reviews)
-            return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
+                    continue
+                
+            db_coll.insert_many(reviews)
+            return render_template('results.html', reviews=reviews[0:len(reviews)])
         except Exception as e:
-            print('The Exception message is: ',e)
             return 'something is wrong'
-    # return render_template('results.html')
-
     else:
         return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000, debug=True)
-	#app.run(debug=True)
